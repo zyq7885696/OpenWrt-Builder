e65e693ae13187b81561c32e6c74ad5a3e33bcf4
zyq7885696 <59403703+zyq7885696@users.noreply.github.com>
2026-05-29 23:13:54 +0800

    Update docker-build.sh

--- immortalwrt/docker-build.sh
+++ immortalwrt/docker-build.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 set -e
 
-# 1. 本地创建 bin 目录并设为 777 权限
 mkdir -p ./bin
 chmod 777 ./bin
 
-# 2. 运行构建容器，去掉容器内的 chmod，直接运行 build.sh
 docker run --rm \
   -v "$(pwd)/bin:/home/build/immortalwrt/bin" \
   -v "$(pwd)/files:/home/build/immortalwrt/files" \
