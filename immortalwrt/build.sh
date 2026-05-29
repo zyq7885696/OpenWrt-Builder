db98c9d67087385545b47ccbe54800a97c08a5dd
zyq7885696 <59403703+zyq7885696@users.noreply.github.com>
2026-05-29 23:07:17 +0800

    Update build.sh

--- immortalwrt/build.sh
+++ immortalwrt/build.sh
@@ -13,9 +13,6 @@ PACKAGES="$PACKAGES curl wget ca-certificates"
 PACKAGES="$PACKAGES luci-i18n-base-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-opkg-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-admin-system-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-admin-status-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-network-zh-cn"
 
 # Argon 主题
 PACKAGES="$PACKAGES luci-theme-argon"
