48eb17c4702011f7e8deeabfc973510bb7b9bc83
zyq7885696 <59403703+zyq7885696@users.noreply.github.com>
2026-05-29 22:59:41 +0800

    Update docker-build.sh

--- immortalwrt/docker-build.sh
+++ immortalwrt/docker-build.sh
@@ -2,10 +2,11 @@
 set -e
 
 mkdir -p ./bin
+chmod 777 ./bin
 
 docker run --rm \
   -v "$(pwd)/bin:/home/build/immortalwrt/bin" \
   -v "$(pwd)/files:/home/build/immortalwrt/files" \
   -v "$(pwd)/build.sh:/home/build/immortalwrt/build.sh" \
-  immortalwrt/imagebuilder:x86-64-openwrt-24.10.1 \
-  /home/build/immortalwrt/build.sh
+  immortalwrt/imagebuilder:x86-64-openwrt-24.10.6 \
+  sh -c "chmod -R 777 /home/build/immortalwrt/bin && /home/build/immortalwrt/build.sh"
